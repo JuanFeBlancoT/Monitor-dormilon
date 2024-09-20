a0cadf47ee43e572ea9be19680ef6cd41fdad410
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2024-09-19 22:36:39 -0500

    Monitor behavior simulated

--- src/Monitor.java
+++ src/Monitor.java
@@ -3,10 +3,46 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Monitor implements Runnable{
 
+	private Semaphore monitorAvailable;
+    private Semaphore chairs;
+    private boolean isSleeping = true;
+
+    public Monitor(Semaphore monitorAvailableM, Semaphore chairsM) {
+        monitorAvailable = monitorAvailableM;
+        chairs = chairsM;
+    }
+    
+    
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		try {
+            while (true) {
+            	
+                // If there aren´t any students asking for help: He sleeps
+                if (chairs.availablePermits() == 3) {
+                    System.out.println("The monitor is sleeping");
+                    isSleeping = true;
+                    monitorAvailable.acquire();
+                }
+
+                // Monitor wakes up and helps someone
+                isSleeping = false;
+                System.out.println("Monitor is helping a student");
+                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 3000)); // Wait simulation
+
+                // Release a chair when the monitor is done with the student
+                chairs.release();
+                System.out.println("El monitor ha terminado de ayudar a un estudiante.");
+                
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 	}
+	
+	public boolean isSleeping() {
+        return isSleeping;
+    }
 	
 }
